f95549276ce2c31210dcce893125405479205336
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-02-26 15:02:03 +0100

    done

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,20 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">so-nuxt-background-image</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <img src="~static/img/info/production/pic4.jpg" class="t22" />
+    <a href="/page/fresco-art" class="btn t23">First text</a>
+
+    <div class="hr t24"></div>
+    <h2 class="t25">Second text</h2>
+
+    <ul class="services">
+      <li
+        :style="{
+          backgroundImage: `url(${require('@/static/img/info/production/pic5.jpg')})`,
+        }"
+        class="sq"
+      >
+        <div class="n">Third text</div>
+      </li>
+    </ul>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
